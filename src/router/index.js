57052145eb776db0a2d62097f6dd2fe0d264dc6a
yuguanpei <yuguanpei@hotmail.com>
2023-01-05 10:21:43 +0800

    fix: reach route after socket is ready

--- src/router/index.js
+++ src/router/index.js
@@ -47,43 +47,49 @@ const router = createRouter({
   },
 });
 
-router.beforeEach(async (to, from, next) => {
-  const modelEl = document.getElementsByClassName('modal show')?.[0];
-  if (modelEl) {
-    modelEl.classList.remove('show');
-    const modelBackdropEl = document.getElementsByClassName('modal-backdrop show')?.[0];
-    if (modelBackdropEl) {
-      setTimeout(() => {
-        modelBackdropEl.parentNode.removeChild(modelBackdropEl);
-      }, 200);
-    }
-  }
-  NProgress.start();
-  const token = jwt.decode(localStorage.getItem('accessToken'));
-  if (token && token.exp > Math.round(new Date() / 1000)) {
-    await checkUserData();
-    if (['login'].includes(to.name)) {
-      next({ name: 'home' });
-    } else {
-      if (['list', 'view', 'edit'].includes(to.name) && store.state.user.forms.some((form) => Number(form.pid) === Number(to.params?.tid))) {
-        next({ name: 'notFound' });
-      } else if (
-        (['list', 'view', 'edit'].includes(to.name) && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.checked)) ||
-        (['edit'].includes(to.name) && Number(to.params?.rid) === 0 && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.create)) ||
-        (['edit'].includes(to.name) && Number(to.params?.rid) !== 0 && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.modify)) ||
-        (to?.meta?.auth && !(store.state.user.data.tags.includes('ALL') || to?.meta?.auth.every((tag) => store.state.user.data.tags.includes(tag))))
-      ) {
-        next({ name: 'permissionDenied' });
+router.beforeEach((to, from, next) => {
+  const interval = setInterval(async () => {
+    if (window.socket) {
+      clearInterval(interval);
+
+      const modelEl = document.getElementsByClassName('modal show')?.[0];
+      if (modelEl) {
+        modelEl.classList.remove('show');
+        const modelBackdropEl = document.getElementsByClassName('modal-backdrop show')?.[0];
+        if (modelBackdropEl) {
+          setTimeout(() => {
+            modelBackdropEl.parentNode.removeChild(modelBackdropEl);
+          }, 200);
+        }
       }
-      next();
+      NProgress.start();
+      const token = jwt.decode(localStorage.getItem('accessToken'));
+      if (token && token.exp > Math.round(new Date() / 1000)) {
+        await checkUserData();
+        if (['login'].includes(to.name)) {
+          next({ name: 'home' });
+        } else {
+          if (['list', 'view', 'edit'].includes(to.name) && store.state.user.forms.some((form) => Number(form.pid) === Number(to.params?.tid))) {
+            next({ name: 'notFound' });
+          } else if (
+            (['list', 'view', 'edit'].includes(to.name) && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.checked)) ||
+            (['edit'].includes(to.name) && Number(to.params?.rid) === 0 && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.create)) ||
+            (['edit'].includes(to.name) && Number(to.params?.rid) !== 0 && !(store.state.user.data.tags.includes('ALL') || store.state.user.data.permissions?.[Number(to.params?.tid)]?.modify)) ||
+            (to?.meta?.auth && !(store.state.user.data.tags.includes('ALL') || to?.meta?.auth.every((tag) => store.state.user.data.tags.includes(tag))))
+          ) {
+            next({ name: 'permissionDenied' });
+          }
+          next();
+        }
+      } else {
+        if (to?.meta?.auth) {
+          // Redirect to login page
+          next({ name: 'login', query: { redirect: to.path } });
+        }
+        next();
+      }
     }
-  } else {
-    if (to?.meta?.auth) {
-      // Redirect to login page
-      next({ name: 'login', query: { redirect: to.path } });
-    }
-    next();
-  }
+  }, 100);
 });
 
 router.afterEach((to) => {
