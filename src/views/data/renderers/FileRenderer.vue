61463af0e32fd66c32318c076005c80b685e244c
yuguanpei <yuguanpei@hotmail.com>
2023-03-20 16:20:55 +0800

    fix: file renderer

--- src/views/data/renderers/FileRenderer.vue
+++ src/views/data/renderers/FileRenderer.vue
@@ -1,42 +1,43 @@
 <template>
   <span v-if="params.value && params.value.length">
-    <div class="avatar-group mt-1">
-      <span
+    <div class="avatar-group">
+      <div
         v-for="(file, index) in params.value"
         :key="index"
-        class="avatar-group-item cursor-pointer"
+        class="avatar-group-item"
+        :title="file.name"
       >
-        <div :title="file.name">
-          <img
-            v-if="file.category == 'image'"
-            :src="`${BASE_URL}cor/file/load/${file.uuid}`"
-            class="avatar-xs cursor-pointer mb-3"
-            @click="
-              handleClickImage(() => {
-                const images = params.value.filter((file) => file.category == 'image');
-                $viewerApi({
-                  options: {
-                    focus: false,
-                    movable: false,
-                    initialViewIndex: images.findIndex((image) => image.uuid == file.uuid),
-                  },
-                  images: images.map((image) => {
-                    return `${BASE_URL}cor/file/load/${image.uuid}`;
-                  }),
-                });
-              })
-            "
-            @dblclick="handleDbclickFile(file)"
-            loading="lazy"
-          />
-          <i
-            v-else
-            class="file-icon mt-n2"
-            :class="$fileIcons.getClassWithColor(file.name)"
-            @dblclick="handleDbclickFile(file)"
-          />
-        </div>
-      </span>
+        <img
+          v-if="file.category == 'image'"
+          :src="`${BASE_URL}cor/file/load/${file.uuid}`"
+          class="bg-light avatar-xs rounded cursor-pointer overflow-hidden"
+          style="box-shadow: 0px 3px 5px 0px rgba(30, 32, 37, 0.3)"
+          @click="
+            handleClickImage(() => {
+              const images = params.value.filter((file) => file.category == 'image');
+              $viewerApi({
+                options: {
+                  focus: false,
+                  movable: false,
+                  initialViewIndex: images.findIndex((image) => image.uuid == file.uuid),
+                },
+                images: images.map((image) => {
+                  return `${BASE_URL}cor/file/load/${image.uuid}`;
+                }),
+              });
+            })
+          "
+          @dblclick="handleDbclickFile(file)"
+          loading="lazy"
+        />
+        <i
+          v-else
+          class="bg-light avatar-xs rounded cursor-pointer overflow-hidden file-icon"
+          style="box-shadow: 0px 3px 5px 0px rgba(30, 32, 37, 0.3)"
+          :class="$fileIcons.getClassWithColor(file.name)"
+          @dblclick="handleDbclickFile(file)"
+        />
+      </div>
     </div>
   </span>
   <span v-else></span>
